232414b22ec7e3ba6872e1618ff732ca2a6ceb7e
israeldail <israeldail2@gmail.com>
2022-07-13 23:22:20 +0000

    trying to fetch info

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,76 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+  return {
+    store: {
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+      getPlanet: [],
+      getPerson: [],
+      getVehicle: [],
+      favorites: [],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+        getPerson: () => {
+          fetch("https://www.swapi.tech/api/people")
+            .then((response) => {
+              if (response.status === 200) {
+                console.log("SUCCESSS");
+                return response.json();
+              } else if (response.status === 408) {
+                console.log("SOMETHING WENT WRONG");
+              }
+            })
+            .then((data) => {
+              console.log(data);
+              setStore({ person: data.results });
+            })
+            .catch((error) => console.log("error has occurred", error));
+        };
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        getPlanet: () => {
+          fetch("https://www.swapi.tech/api/planets")
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              setStore({ planet: data.results });
+            })
+            .catch((error) => console.log("error has occurred", error));
+        };
+      },
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import injectContext from "../store/appContext";
 
-export const Home = () => (
+export const Home = () => {
+	
+
+	return(
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>  </h1>
 	</div>
-);
+	)
+};
+
+
