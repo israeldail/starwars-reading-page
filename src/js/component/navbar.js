93c29c3c0e06770fad0d8f73eb5a95f9e429868e
izzy417 <israeldail2@gmail.com>
2022-08-14 18:25:10 +0000

    added drop down

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export const Navbar = () => {
-	return (
-		<nav id="logo" className="navbar navbar-light bg-dark mb-3">
-			<img src="https://cdn.mos.cms.futurecdn.net/HoV9PTARj4sSSKHq8ScVFB.jpg" height="100" width="200"></img>
-			<button id="navButton">Favorites <span id="navNumber">0</span></button>
-		</nav>
-	);
+  return (
+    <nav id="logo" className="navbar navbar-light bg-dark mb-3">
+      <img
+        src="https://cdn.mos.cms.futurecdn.net/HoV9PTARj4sSSKHq8ScVFB.jpg"
+        height="100"
+        width="200"
+      ></img>
+      <div class="dropdown">
+        <button className="navButton" id="dropbtn">
+          Favorites <span id="navNumber">0</span>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </nav>
+  );
 };
